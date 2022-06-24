96f276117584b9409498595c39bd5097c4929aa1
João Pinto <6815379+pinjoa@users.noreply.github.com>
2022-06-24 22:30:00 +0100

    inicialização
    template relatório LaTeX

---
+++ Templates/Relatorio LaTeX/tex-clear-temp-files.bat
@@ -0,0 +1,9 @@
+del *.bak 
+del *.ps 
+del *.dvi 
+del *.aux 
+del *.idx 
+del *.ind 
+del *.ilg 
+del *.ist
+del *.log *.out *.brf *.blg *.bbl *.gz *.acn *.glo *.lof *.lot *.lol *.toc *.glsdefs *.acr *.alg *.glg *.gls
